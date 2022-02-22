51f46c4d0ecd7bac0ad56ad4a0fc5f74bec03fdd
Igor dev <devserigor@gmail.com>
2022-02-22 05:40:13 -0300

    inicio projeto true front

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,19 @@
+export default {
+  title: "dark",
+
+  colors: {
+    primary: "#111111",
+    secondary: "#1f1f1f",
+    tertiary: "#121214",
+
+    white: "#FFF",
+    black: "#000",
+    gray: "#BFBFBF",
+    green: "#08f036",
+    green30perc: "#08f0364c",
+
+    success: "#4E41F0",
+    info: "#F7931B",
+    warning: "#E44C4E",
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,19 @@
+export default {
+  title: "light",
+
+  colors: {
+    primary: "#DCDCDC",
+    secondary: "#FFFFFF",
+    tertiary: "#F5F5F5",
+
+    white: "#000",
+    black: "#FFF",
+    gray: "#BFBFBF",
+    green: "#08f036",
+    green30perc: "#08f0364c",
+
+    success: "#03BB85",
+    info: "#3B5998",
+    warning: "#FF6961",
+  },
+};
