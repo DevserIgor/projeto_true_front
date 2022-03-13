bdbe147dcc2e56af6f1f955b0fd1dbd16a98f65a
Jonatã <jonatahofmann@gmail.com>
2022-03-13 02:01:21 -0300

    feat: implementação da rota de estrelas random

---
+++ src/pages/StarIframe/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+export const StarNote = styled.div``;
